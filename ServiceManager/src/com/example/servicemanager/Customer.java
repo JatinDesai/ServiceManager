caea0e1dd814400f0f1ed4b2ee4c2e4b624fb297
Pragnesh Patel <pragnesh.patel@amd.ibaset.com>
2016-12-09 08:41:47 +0530

    Fixed copy paste errors

--- ServiceManager/src/com/example/servicemanager/Customer.java
+++ ServiceManager/src/com/example/servicemanager/Customer.java
@@ -73,19 +73,19 @@ public class Customer implements Parcelable {
   }
 
   public void setEmailId(String emailId) {
-    this.address = emailId;
+    this.emailId = emailId;
   }
 
   public String getContactNo() {
     return contactNo;
   }
 
-  public void setContactNo(String mobile) {
-    this.contactNo = mobile;
+  public void setContactNo(String contactNo) {
+    this.contactNo = contactNo;
   }
 
   public String getProductName() {
-    return productModelNo;
+    return productName;
   }
 
   public void setProductName(String productName) {
